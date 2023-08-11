8810031f8e4c6d032b03b00d49d7da54e520cd78
jeferson-viana <jefersonv28@gmail.com>
2023-08-11 15:47:14 -0300

    Commit Initial

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 
 app.use(express.json());
+
+app.get(
+  '/',
+  (_req: Request, res: Response) => res.status(201).json({ message: 'Aplicação funcionando' }),
+);
 
 export default app;
